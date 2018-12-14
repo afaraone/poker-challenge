f968a1e4f1183a103b1b057e192823e327c6eca4
afaraone <andresfaraone@outlook.com>
2018-12-14 17:15:47 +0000

    add Play button to GameForm
    this does cb to parent component with
    values of numPlayers and numCards

---
+++ src/components/gameForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class GameForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numPlayers: null,
+      numCards: null
+    }
+  }
+
+  setNumPlayers(numString) {
+    this.setState({
+      numPlayers: parseInt(numString)
+    });
+  };
+
+  setNumCards(numString) {
+    this.setState({
+      numCards: parseInt(numString)
+    });
+  };
+
+  render() {
+    let numPlayers = this.state.numPlayers;
+    let numCards = this.state.numCards;
+
+    return(
+      <div>
+        <input id="num-players-text"
+        type="text" onChange={e => this.setNumPlayers(e.target.value)}></input>
+
+        <input id="num-cards-text"
+        type="text" onChange={e => this.setNumCards(e.target.value)}></input>
+
+        <button id="play-btn" onClick={this.props.play(numPlayers, numCards)}>
+          Play
+        </button>
+      </div>
+    )
+  }
+};
+
+export default GameForm;
--- src/components/gameForm.test.js
+++ src/components/gameForm.test.js
@@ -4,9 +4,10 @@ import GameForm from './gameForm';
 
 describe('GameForm', () => {
   let wrapper
+  let mockPlay = jest.fn();
 
   beforeEach(() => {
-    wrapper = shallow(<GameForm />);
+    wrapper = shallow(<GameForm play={mockPlay}/>);
   });
 
   it('numPlayers input updates this.state.numPlayers', () => {
@@ -18,4 +19,13 @@ describe('GameForm', () => {
     wrapper.find('#num-cards-text').simulate('change', {target: {value: '5'}})
     expect(wrapper.state('numCards')).toEqual(5)
   });
+
+  it('Play button does cb to prop.play with numPlayers and numCards as args', () => {
+    // set up with mock prop fn and state
+    wrapper.find('#num-players-text').simulate('change', {target: {value: '4'}})
+    wrapper.find('#num-cards-text').simulate('change', {target: {value: '5'}})
+    wrapper.find('#play-btn').simulate('click');
+
+    expect(mockPlay).toHaveBeenCalledWith(4, 5);
+  })
 });
