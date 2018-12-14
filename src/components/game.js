036a430975248d36baf2d7514fe8c6dd2d9f818f
afaraone <andresfaraone@outlook.com>
2018-12-14 17:38:00 +0000

    Game.play calls deal and saves to state

--- src/components/game.js
+++ src/components/game.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import GameForm from './gameForm';
+import {deal} from '../deck';
 
 class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      players: null
     };
   };
 
   play(numPlayers, numCards) {
-    
+    this.setState({
+      players: deal(numPlayers, numCards)
+    });
   };
 
   render() {
     return (
       <>
-        <GameForm play={(numPlayers, numCards) => this.play}/>
+        <GameForm play={(numPlayers, numCards) => this.play(numPlayers, numCards)}/>
       </>
     )
   };
--- src/components/game.test.js
+++ src/components/game.test.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Game from './game';
 import GameForm from './gameForm';
+import * as deck from '../deck'
+import {mockDealtCards} from '../mocks'
+
+// mock deal
+jest.spyOn(deck, 'deal').mockImplementation(() => mockDealtCards);
 
 describe('Game - unit test', () => {
   let wrapper;
@@ -11,5 +16,13 @@ describe('Game - unit test', () => {
 
   it('renders gameForm component', () => {
     expect(wrapper.containsMatchingElement(<GameForm/>)).toEqual(true)
+  });
+
+  describe('#play', () => {
+    it('calls deal with arguments and saves to players', () => {
+      wrapper.instance().play(3, 4);
+      expect(deck.deal).toHaveBeenCalledWith(3, 4);
+      expect(wrapper.state('players')).toEqual(mockDealtCards);
+    });
   });
 });
