d580d9c7f5c127bff47e085bbf72b26191bdff9e
afaraone <andresfaraone@outlook.com>
2018-12-15 15:42:20 +0000

    fix player for travis test

--- src/components/player.js
+++ src/components/player.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 class Player extends Component {
+  constructor(props) {
+    super(props);
+  };
+
   render() {
     return(
+      <div>
+      </div>
     )
   }
 }
